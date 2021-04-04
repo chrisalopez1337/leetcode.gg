d37a60400b79364a2ec7905d600dc9f9cc83c91e
chrisalopez1337 <chrisalopez@outlook.com>
2021-04-03 17:48:02 -0700

    Added function to log in a user, log out a user, as well as a function to fetch the users data by username or email and set it to the userData state

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect, useContext } from 'react';
 import styled from 'styled-components';
+// Utils
+import { setOne, getOne, deleteOne } from 'local-js';
 // Children components
 import Header from './Header.jsx';
 import LandingDisplay from './LandingDisplay.jsx';
 import SignUp from './SignUp.jsx';
 import LogIn from './LogIn.jsx';
+
 
 
 // Styling
@@ -15,8 +18,45 @@ const Container = styled.div`
 `;
 
 export default function App() {
-    const [page, setPage] = useState('home');
+    // User management
+    const [userData, setUserData] = useState(null);
+    // GET users data
+    function fetchAndUpdateUser(username) {
+        // Retrieve user data
+        axios.get(`/api/users/get/${username}`).
+            then(({ data }) => {
+                if (!data?.username) {
+                    setUserData(null);
+                } else {
+                    setUserData(data);
+                }
+            })
+            .catch(console.log);
+    }
+
+    // Persist username in local storage.
+    function logIn(username) {
+        // Set item to local storage
+        const key = 'current-user';
+        setOne(key, username);
+        // Mount user data
+        fetchAndUpdateUser(username);
+    }
+
+    // Log a user out
+    function logOut() {
+        // Remove user from local stroage
+        const key = 'current-user';
+        deleteOne(key);
+        // Remove there user data from state
+        setUserData(null);
+    }
+
+    
+
+
     // Conditional rendering
+    const [page, setPage] = useState('landing');
     const pageRender = page === 'landing'
         ? <LandingDisplay setPage={setPage} />
         : page === 'sign-up'
