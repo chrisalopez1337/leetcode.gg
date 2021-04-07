ceb6d0a0f7b7fcf034fe03b20cc3f5da1e85b39e
chrisalopez1337 <chrisalopez@outlook.com>
2021-04-06 17:27:33 -0700

    Added formatRunCommand and tests for it to the EvalCode class, all passing

--- testing/handleCode.test.js
+++ testing/handleCode.test.js
@@ -3,6 +3,7 @@ const assert = require('assert');
 const EvalCode = require('../utils/codeHandler.js');
 
 describe('EvalCode class', async () => {
+    // JS
     it('Should be able to format a file name properly', () => {
         const code = `const returnArr = (arr) => arr;`
         const testCaseInfo = { expected: [1, 2, 3], case: `returnArr({1, 2, 3})` };
@@ -11,6 +12,7 @@ describe('EvalCode class', async () => {
         e.formatFileName();
         expect(e.get('fileName')).to.equal('eval.js');
     });
+    //Python
     it('Should be able to format a file name properly', () => {
         const code = 
             `def getX(x):
@@ -20,5 +22,26 @@ describe('EvalCode class', async () => {
         const e = new EvalCode(code, language, testCaseInfo);
         e.formatFileName();
         expect(e.get('fileName')).to.equal('eval.py');
+    });
+
+    //JS
+    it('Should be able to format a run command properly', () => {
+        const code = `const returnArr = (arr) => arr;`
+        const testCaseInfo = { expected: [1, 2, 3], case: `returnArr({1, 2, 3})` };
+        const language = 'javascript';
+        const e = new EvalCode(code, language, testCaseInfo);
+        e.__init__();
+        expect(e.get('command')).to.equal('node eval.js');
+    });
+    //Python
+    it('Should be able to format a run command properly', () => {
+        const code = 
+            `def getX(x):
+                return x`;
+        const testCaseInfo = { expected: 1, case: `getX(1)` };
+        const language = 'python';
+        const e = new EvalCode(code, language, testCaseInfo);
+        e.__init__();
+        expect(e.get('command')).to.equal('python3 eval.py');
     });
 });
--- utils/codeHandler.js
+++ utils/codeHandler.js
@@ -17,6 +17,11 @@ class EvalCode {
         return this[key];
     }
 
+    __init__() {
+        this.formatFileName();
+        this.formatRunCommand();
+    }
+
     formatFileName() {
         const languageOptions = 
             {
@@ -25,7 +30,23 @@ class EvalCode {
             };
         const key = this.language;
         const extension = languageOptions[key];
-        this.fileName = 'eval' + extension;
+        this.fileName = `eval${extension}`;
+    }
+
+    formatRunCommand() {
+        // Cant be run without a valid fileName
+        if (!this.fileName) {
+            throw new Error('Need to run: formatFileName prior to building a run command.');
+            return;
+        }
+        const languageOptions =
+            {
+                javascript: 'node',
+                python: 'python3',
+            }
+        const key = this.language;
+        const command = languageOptions[key];
+        this.command = `${command} ${this.fileName}`;
     }
 }
 
